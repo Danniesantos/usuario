c51aaafebb99003be48c639e8140b32962562a1f
Danniesantos <danielardesantos@gmail.com>
2025-10-30 17:22:55 -0300

    Criado e efetuado testes de viaCepDTO

--- src/main/java/com/daniela/usuario/business/ViaCepService.java
+++ src/main/java/com/daniela/usuario/business/ViaCepService.java
@@ -2,6 +2,7 @@ package com.daniela.usuario.business;
 
 import com.daniela.usuario.infrastructure.security.client.ViaCepClient;
 import com.daniela.usuario.infrastructure.security.client.ViaCepDTO;
+import com.daniela.usuario.infrastructure.exceptions.IllegalArgumentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class ViaCepService {
         try {
             return viaCepClient.buscaDadosEndereco(processarCep(cep));
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Erro: ", e);
+            throw new IllegalArgumentException("O cep contém caracteres inválidos");
         }
     }
 
--- src/main/java/com/daniela/usuario/infrastructure/security/client/ViaCepDTO.java
+++ src/main/java/com/daniela/usuario/infrastructure/security/client/ViaCepDTO.java
@@ -2,11 +2,14 @@ package com.daniela.usuario.infrastructure.security.client;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class ViaCepDTO {
 
     private String cep;
@@ -22,5 +25,30 @@ public class ViaCepDTO {
     private String gia;
     private String ddd;
     private String siafi;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ViaCepDTO dto)) return false;
+        return Objects.equals(cep, dto.cep) &&
+                Objects.equals(logradouro, dto.logradouro) &&
+                Objects.equals(complemento, dto.complemento) &&
+                Objects.equals(unidade, dto.unidade) &&
+                Objects.equals(bairro, dto.bairro) &&
+                Objects.equals(localidade, dto.localidade) &&
+                Objects.equals(uf, dto.uf) &&
+                Objects.equals(estado, dto.estado) &&
+                Objects.equals(regiao, dto.regiao) &&
+                Objects.equals(ibge, dto.ibge) &&
+                Objects.equals(gia, dto.gia) &&
+                Objects.equals(ddd, dto.ddd) &&
+                Objects.equals(siafi, dto.siafi);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cep, logradouro, complemento, unidade, bairro, localidade, uf, estado,
+                regiao, ibge, gia, ddd, siafi);
+    }
 
 }
